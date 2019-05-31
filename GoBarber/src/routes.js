a1d27c9f78ec0f1207450b790a77112469017fc7
Carlos Schuenck <carlosschuenck@hotmail.com>
2019-05-31 00:15:43 -0300

    login e logout

--- GoBarber/src/routes.js
+++ GoBarber/src/routes.js
@@ -4,15 +4,27 @@ const upload = require('multer')(multerConfig)
 const routes = express.Router()
 const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
+const authMiddleware = require('./app/middlewares/auth')
+const guestMiddleware = require('./app/middlewares/guest')
 
-routes.get('/', SessionController.create)
+routes.use((req, res, next) => {
+  res.locals.flashSuccess = req.flash('success')
+  res.locals.flashError = req.flash('error')
+
+  return next()
+})
+
+routes.get('/', guestMiddleware, SessionController.create)
 routes.post('/signin', SessionController.store)
 
-routes.get('/signup', UserController.create)
+routes.get('/signup', guestMiddleware, UserController.create)
 routes.post('/signup', upload.single('avatar'), UserController.store)
 
+routes.use('/app', authMiddleware)
 routes.get('/app/dashboard', (req, res) => {
   console.log(req.session.user)
   res.render('dashboard')
 })
+
+routes.get('/app/logout', SessionController.destroy)
 module.exports = routes
